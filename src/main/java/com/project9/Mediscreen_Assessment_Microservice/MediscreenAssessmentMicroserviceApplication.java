c24c44e03df640952ff2381b4721c75fd7717581
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-29 17:10:23 +0100

    Edit MediscreenAssessmentMicroserviceApplication -> add @Annotation @ComponentScan(com.project9.Mediscreen_Note_Microservice)

--- src/main/java/com/project9/Mediscreen_Assessment_Microservice/MediscreenAssessmentMicroserviceApplication.java
+++ src/main/java/com/project9/Mediscreen_Assessment_Microservice/MediscreenAssessmentMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.project9.Mediscreen_Assessment_Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.project9.Mediscreen_Note_Microservice") // to scan packages mentioned
+//@EnableMongoRepositories("com.project9.Mediscreen_Note_Microservice") // to activate MongoDB repositories
 public class MediscreenAssessmentMicroserviceApplication {
 
 	public static void main(String[] args) {
