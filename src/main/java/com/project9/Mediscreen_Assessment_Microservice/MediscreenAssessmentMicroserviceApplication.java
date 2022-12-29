463c716aee2486879002bdaf1d62993425677527
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-29 22:48:26 +0100

    Transform into eurekaclient to connect eurekaserver

--- src/main/java/com/project9/Mediscreen_Assessment_Microservice/MediscreenAssessmentMicroserviceApplication.java
+++ src/main/java/com/project9/Mediscreen_Assessment_Microservice/MediscreenAssessmentMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.project9.Mediscreen_Assessment_Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.project9.Mediscreen_Note_Microservice") // to scan packages mentioned
+@EnableDiscoveryClient
 //@EnableMongoRepositories("com.project9.Mediscreen_Note_Microservice") // to activate MongoDB repositories
 public class MediscreenAssessmentMicroserviceApplication {
 
